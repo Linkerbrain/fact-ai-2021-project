f892555576b17b955aebf1d2977c06c2323794a5
Alfonso Taboada <avtwarmerdam@gmail.com>
2022-01-15 22:22:53 +0100

    I believe the code given to has had an error in bencharm_attack.sh, this is a fix.

--- ATSPrivacy-Framework/benchmark/run/benchmark_attack.sh
+++ ATSPrivacy-Framework/benchmark/run/benchmark_attack.sh
@@ -3,7 +3,7 @@ export CUDA_VISIBLE_DEVICES=0
 data=cifar100
 epoch=200
 arch='ResNet20-4'
-python benchmark/cifar100_train.py --data=$data --arch=$arch --epochs=$epoch --aug_list='' --mode=aug 
+python benchmark/cifar100_train.py --data=$data --arch=$arch --epochs=$epoch --aug_list='' --mode=normal
 python benchmark/cifar100_attack.py --data=$data --arch=$arch --epochs=$epoch --aug_list='' --mode=normal --optim='inversed'
 
 for aug_list in '3-1-7+43-18-18' '3-1-7' '43-18-18';
