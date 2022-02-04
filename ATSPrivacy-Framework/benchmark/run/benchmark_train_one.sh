294cb2fae2a9fe5095a26d3f9f8d487fc3622430
Alfonso Taboada <avtwarmerdam@gmail.com>
2022-02-04 18:41:27 +0100

    Prepare example notebook

---
+++ ATSPrivacy-Framework/benchmark/run/benchmark_train_one.sh
@@ -0,0 +1,16 @@
+
+export CUDA_VISIBLE_DEVICES=0
+data=cifar100
+epoch=1
+arch='ResNet20-4'
+python benchmark/cifar100_train.py --data=$data --arch=$arch --epochs=$epoch --aug_list='' --mode=normal
+#python benchmark/cifar100_train.py --data=$data --arch=$arch --epochs=$epoch --aug_list='' --mode=crop
+#python benchmark/cifar100_train.py --data=$data --arch=$arch --epochs=$epoch --aug_list='' --mode=aug
+
+for aug_list in '3-1-7+43-18-18' '3-1-7' '43-18-18';
+do
+{
+echo $aug_list
+python -u benchmark/cifar100_train.py --data=$data --arch=$arch --epochs=$epoch --aug_list=$aug_list --mode=aug
+}
+done
